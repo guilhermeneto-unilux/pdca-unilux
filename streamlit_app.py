0e3d2c8bfb9ba8da5f009c86548659bb4f1f39ec
Guilherme Neto <guilherme.neto@unilux.com.br>
2026-04-02 16:07:04 -0300

    Ultra minimal setup to fix blank screen (staging)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
 import streamlit as st
-import os
-
-# Este arquivo serve para garantir que o Streamlit Cloud encontre a aplicação
-# caso o "Main file path" esteja configurado como 'streamlit_app.py'
-
-if os.path.exists("app.py"):
-    exec(open("app.py", encoding="utf-8").read())
-else:
-    st.error("Erro Crítico: Arquivo app.py não encontrado no repositório.")
+st.write("Rodando via streamlit_app.py")
+import app
