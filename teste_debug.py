f23a1fa56c2b748d9a0079244cc3d753e2a12a71
Guilherme Neto <guilherme.neto@unilux.com.br>
2026-04-02 15:58:12 -0300

    Create debug file (staging)

---
+++ teste_debug.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Debug Unilux", page_icon="🏗️")
+
+st.title("🏗️ UNILUX DEBUG MODE")
+st.write("---")
+st.success("O ambiente está funcionando perfeitamente!")
+st.info("Se você está vendo esta mensagem, o problema é algo específico dentro do arquivo `app.py` original.")
+st.write("Diga-me aqui no chat: 'Eu consegui ver o Debug!'")
